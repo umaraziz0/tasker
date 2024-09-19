657b057a6c33839aee6a8b50ae94c7e7e543c52d
Aziz <mu.aziz97@yahoo.com>
2024-09-19 22:10:33 +0700

    build: setup typescript, install `husky`, `lint-staged`, and `commitlint`

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
+
   devtools: { enabled: true },
+
+  typescript: {
+    typeCheck: true,
+  },
+
   modules: ['@nuxt/eslint', '@nuxtjs/tailwindcss'],
 });
